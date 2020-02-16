393c47edd943cc909f8ec5352ec1711194dd60fe
Sachin796 <sachinjaveri.2010@gmail.com>
2020-02-16 02:51:00 -0500

    topleft images rotated

---
+++ public/rotate.js
@@ -0,0 +1,13 @@
+var divleft = document.getElementsByClassName("circletopleft");
+var divright = document.getElementsByClassName("circletopright");
+var angle = 0;
+document.onwheel = function(e) {
+  if (e.deltaY) {
+    console.log(e);
+    // we have a wheel for vertical (common) direction
+    e.preventDefault();
+    angle += e.deltaY < 0 ? 10 : -10; // what direction?
+    divleft[0].style.transform = "rotate(" + angle + "deg)"; // do something
+    divright[0].style.transform = "rotate(" + angle + "deg)"; // do something
+  }
+};
--- src/components/circle/index.js
+++ src/components/circle/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Circle(props) {
-  return <div className={props.className} id={props.id}></div>;
+  return (
+    <div className={props.className} id={props.id}>
+      Topleft/Top right
+    </div>
+  );
 }
 
 export default Circle;
