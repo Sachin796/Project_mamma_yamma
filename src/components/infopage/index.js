84912fc6d87497ce36ba8a356a2a7f4f85bfbc91
Sachin796 <sachinjaveri.2010@gmail.com>
2020-02-25 14:22:04 -0500

    changes to border

--- src/components/infopage/index.js
+++ src/components/infopage/index.js
@@ -5,9 +5,22 @@ import Backimg from "../images/infopageimg1.png";
 import LeftBorder from "../images/border/infopage_left.png";
 import RightBorder from "../images/border/infopage_right.png";
 import TopBottomBorder from "../topbottomborder/index";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 function InfoPage() {
   return (
     <>
+      {/* <Container>
+        <Row>
+          <Col md={8} sm={4} xs={2}>
+            <div id="left_right_border">
+              <img src={props.src} id="leftrightimg1"></img>
+            </div>
+          </Col>
+        </Row>
+      </Container> */}
       <TopBottomBorder></TopBottomBorder>
       <div id="rootdiv1">
         {/* <div className="lrroot"> */}
