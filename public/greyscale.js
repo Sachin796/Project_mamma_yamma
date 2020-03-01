fed11bf18826141129e97f0c6e5bdccf68786014
Sachin796 <sachinjaveri.2010@gmail.com>
2020-02-29 20:28:35 -0500

    responsive breakpoints

--- public/greyscale.js
+++ public/greyscale.js
@@ -1,79 +1,79 @@
-window.addEventListener("load", function() {
-  init();
+// window.addEventListener("load", function() {
+//   init();
 
-  function init() {
-    var url =
-      "https://sherwin.scene7.com/is/image/sw/color-swatch?_tparam_size=250,250&layer=comp&_tparam_color=C2C0BB";
+//   function init() {
+//     var url =
+//       "https://sherwin.scene7.com/is/image/sw/color-swatch?_tparam_size=250,250&layer=comp&_tparam_color=C2C0BB";
 
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
+//     var canvas = document.getElementById("canvas");
+//     var ctx = canvas.getContext("2d");
 
-    var img = new Image();
-    img.src = url;
-    img.crossOrigin = "Anonymous";
+//     var img = new Image();
+//     img.src = url;
+//     img.crossOrigin = "Anonymous";
 
-    img.onload = function() {
-      var width = window.screen.width;
-      var height = window.screen.height;
+//     img.onload = function() {
+//       var width = window.screen.width;
+//       var height = window.screen.height;
 
-      canvas.width = width;
-      canvas.height = height;
-      ctx.drawImage(img, 0, 0, width, height);
-    };
+//       canvas.width = width;
+//       canvas.height = height;
+//       ctx.drawImage(img, 0, 0, width, height);
+//     };
 
-    function drawPoint(pointX, pointY) {
-      var imageData = ctx.getImageData(
-        0,
-        0,
-        ctx.canvas.width,
-        ctx.canvas.height
-      );
-      imageData.crossOrigin = "Anonymous";
+//     function drawPoint(pointX, pointY) {
+//       var imageData = ctx.getImageData(
+//         0,
+//         0,
+//         ctx.canvas.width,
+//         ctx.canvas.height
+//       );
+//       imageData.crossOrigin = "Anonymous";
 
-      ctx.globalCompositeOperation = "destination-out";
-      ctx.arc(pointX, pointY, 45, 0, Math.PI * 2);
-      ctx.fill();
-      ctx.lineWidth = 20;
-      ctx.beginPath();
-      ctx.stroke();
-      ctx.filter = "blur(1px)";
-      var trans = 0;
-      for (var i = 0; i < imageData.data.length; i += 4) {
-        if (imageData.data[i + 3] <= 10) {
-          trans++;
-        }
-      }
-      var total = imageData.data.length / 4;
-      var percent = (trans / total) * 100;
-      if (percent >= 75) {
-        document.getElementById("canvas").style.display = "none";
-      }
-    }
+//       ctx.globalCompositeOperation = "destination-out";
+//       ctx.arc(pointX, pointY, 45, 0, Math.PI * 2);
+//       ctx.fill();
+//       ctx.lineWidth = 20;
+//       ctx.beginPath();
+//       ctx.stroke();
+//       ctx.filter = "blur(1px)";
+//       var trans = 0;
+//       for (var i = 0; i < imageData.data.length; i += 4) {
+//         if (imageData.data[i + 3] <= 10) {
+//           trans++;
+//         }
+//       }
+//       var total = imageData.data.length / 4;
+//       var percent = (trans / total) * 100;
+//       if (percent >= 75) {
+//         document.getElementById("canvas").style.display = "none";
+//       }
+//     }
 
-    canvas.addEventListener(
-      "touchstart",
-      function(e) {
-        drawPoint(e.touches[0].screenX, e.touches[0].screenY);
-      },
-      false
-    );
+//     canvas.addEventListener(
+//       "touchstart",
+//       function(e) {
+//         drawPoint(e.touches[0].screenX, e.touches[0].screenY);
+//       },
+//       false
+//     );
 
-    canvas.addEventListener(
-      "touchmove",
-      function(e) {
-        e.preventDefault();
-        drawPoint(e.touches[0].screenX, e.touches[0].screenY);
-      },
-      false
-    );
+//     canvas.addEventListener(
+//       "touchmove",
+//       function(e) {
+//         e.preventDefault();
+//         drawPoint(e.touches[0].screenX, e.touches[0].screenY);
+//       },
+//       false
+//     );
 
-    canvas.addEventListener(
-      "mousemove",
-      function(e) {
-        e.preventDefault();
-        drawPoint(e.screenX, e.screenY);
-      },
-      false
-    );
-  }
-});
+//     canvas.addEventListener(
+//       "mousemove",
+//       function(e) {
+//         e.preventDefault();
+//         drawPoint(e.screenX, e.screenY);
+//       },
+//       false
+//     );
+//   }
+// });
--- public/rotate.js
+++ public/rotate.js
@@ -1,97 +1,95 @@
-// var divleft = document.getElementsByClassName("face");
-var divright = document.getElementsByClassName("face2");
-var cardDiv = document.getElementsByClassName("cardi");
-var angle = 0;
-var angle2 = 0;
-var i = 0
-document.onwheel = function (e) {
-  if (e.deltaY > 0) {
+// // var divleft = document.getElementsByClassName("face");
+// var divright = document.getElementsByClassName("face2");
+// var cardDiv = document.getElementsByClassName("cardi");
+// var angle = 0;
+// var angle2 = 0;
+// var i = 0
+// document.onwheel = function (e) {
+//   if (e.deltaY > 0) {
 
-    i++
-    if(i >= 1000)
-    {
-      i = 0;
-    }
+//     i++
+//     if(i >= 1000)
+//     {
+//       i = 0;
+//     }
 
-    if(i <= 0)
-    {
-      i = 0;
-    }
+//     if(i <= 0)
+//     {
+//       i = 0;
+//     }
 
-    // angle += e.deltaY < 0 ? 10 : -10;
-    angle2 += e.deltaY < 0 ? -10 : 10;
+//     // angle += e.deltaY < 0 ? 10 : -10;
+//     angle2 += e.deltaY < 0 ? -10 : 10;
 
-    // divleft[0].style.transform = "rotate(" + angle + "deg)";
-    divright[0].style.transform = "rotate(" + angle2 + "deg)";
+//     // divleft[0].style.transform = "rotate(" + angle + "deg)";
+//     divright[0].style.transform = "rotate(" + angle2 + "deg)";
 
-    
-    cardDiv[0].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[1].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[2].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[3].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[4].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[5].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[6].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[7].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[8].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[9].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[10].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[11].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[12].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[13].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[14].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[15].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[16].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[17].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[18].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[19].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[20].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[21].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[22].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[23].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[24].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[25].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[26].style.transform = "translateX(-" + i * 2 + "%)";
-    cardDiv[27].style.transform = "translateX(-" + i * 2 + "%)";
-  }
-  else
-  {
-    i--
+//     cardDiv[0].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[1].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[2].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[3].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[4].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[5].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[6].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[7].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[8].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[9].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[10].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[11].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[12].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[13].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[14].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[15].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[16].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[17].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[18].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[19].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[20].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[21].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[22].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[23].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[24].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[25].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[26].style.transform = "translateX(-" + i * 2 + "%)";
+//     cardDiv[27].style.transform = "translateX(-" + i * 2 + "%)";
+//   }
+//   else
+//   {
+//     i--
 
-    // angle += e.deltaY < 0 ? 10 : -10;
-    angle2 += e.deltaY < 0 ? -10 : 10;
+//     // angle += e.deltaY < 0 ? 10 : -10;
+//     angle2 += e.deltaY < 0 ? -10 : 10;
 
-    // divleft[0].style.transform = "rotate(" + angle + "deg)";
-    divright[0].style.transform = "rotate(" + angle2 + "deg)";
+//     // divleft[0].style.transform = "rotate(" + angle + "deg)";
+//     divright[0].style.transform = "rotate(" + angle2 + "deg)";
 
-    
-      cardDiv[0].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[1].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[2].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[3].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[4].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[5].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[6].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[7].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[8].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[9].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[10].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[11].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[12].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[13].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[14].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[15].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[16].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[17].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[18].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[19].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[20].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[21].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[22].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[23].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[24].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[25].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[26].style.transform = "translateX(-" + i * 2 + "%)";
-      cardDiv[27].style.transform = "translateX(-" + i * 2 + "%)";
-  }
-};
+//       cardDiv[0].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[1].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[2].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[3].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[4].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[5].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[6].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[7].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[8].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[9].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[10].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[11].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[12].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[13].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[14].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[15].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[16].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[17].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[18].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[19].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[20].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[21].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[22].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[23].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[24].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[25].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[26].style.transform = "translateX(-" + i * 2 + "%)";
+//       cardDiv[27].style.transform = "translateX(-" + i * 2 + "%)";
+//   }
+// };
