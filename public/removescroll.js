d828821f63e38f346c29bf04fa0ec95c02192d21
Sachin796 <sachinjaveri.2010@gmail.com>
2020-03-01 03:10:31 -0500

    mobile view design  and bottom card number limited in mobie view

---
+++ public/removescroll.js
@@ -0,0 +1,28 @@
+var scrollListenerActive = false;
+var handleScrollListener = function() {
+  if (window.innerWidth < 786 && !scrollListenerActive) {
+    console.log(`Fired when innerwidth ${window.innerWidth}`);
+    // console.log(document.getElementsByClassName("cardi")[1].classList);
+    document.getElementById("maindiv").style.overflow = "visible";
+    document.getElementById("maindiv").style.overflowY = "scroll";
+    let cardsObj = document.getElementsByClassName("cardi");
+    for (let i = 6; i <= 29; i++) {
+      cardsObj[i].style.display = "none";
+
+      // document.getElementsByClassName("cardi")[i].className = "cardii";
+    }
+    scrollListenerActive = true;
+  } else {
+    document.getElementById("maindiv").style.overflow = "hidden";
+    document.getElementById("maindiv").style.overflowY = "hidden";
+    let cardsObj = document.getElementsByClassName("cardi");
+    for (let i = 6; i <= 29; i++) {
+      cardsObj[i].style.display = "inline-block";
+      // document.getElementsByClassName("cardii")[i].className = "cardi";
+    }
+    scrollListenerActive = false;
+  }
+  scrollListenerActive = false;
+};
+document.addEventListener("load", handleScrollListener); // attach the listener on page load
+window.onresize = handleScrollListener; // attach/remove listener on window resize
